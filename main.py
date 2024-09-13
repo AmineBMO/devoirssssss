b0b34942de109f255a45c6766f9324921591c1ce
AmineBMO <2443562@cstjean.qc.ca>
2024-09-13 18:48:25 -0400

    Fin du projets

--- main.py
+++ main.py
@@ -33,3 +33,4 @@ print(f"KA: {'Surface (m²) :':<34} {surface_metres_carres:>10.1f} m²")
 print(f"KA: {'Temps pour tondre le gazon :':<33} {temps_minutes:>10.1f} minutes")
 print(f"KA: {'Nombre de sacs d\'engrais nécessaires :':<29} {round(nombre_sacs):>2} sacs")
 print(f"KA: {'Coût total des sacs d\'engrais :':<25} {cout_total_apres_taxe:>13.2f}$")
+
